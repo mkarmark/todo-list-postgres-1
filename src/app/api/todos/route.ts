1ec1fe74dbbeef4a2afbab7c7325d4be8f45183b
Mika Marmar <mikarmar@microsoft.com>
2025-06-24 14:49:56 -0700

    refactor: use env vars for local Postgres connection string

--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -4,7 +4,7 @@ import { PrismaClient } from '@/generated/prisma/client';
 const prisma = new PrismaClient({
   datasources: {
     db: {
-      url: "postgresql://todo-list-postgres-db-2--mkuid060700v11nl--user@blackwave-38f9a0e0-westus2-pgsqlserver:ls96TRjx2uylSiUjijQc@blackwave-38f9a0e0-westus2-pgsqlserver.postgres.database.azure.com:5432/todo-list-postgres-db-2--mkuid060700v11nl--db?sslmode=require"
+      url: process.env.DATABASE_URL
     }
   }
 });
